947a0237318202fd53def4d13c32b0167dad7774
EmmJaeTechDevs <ejtechsolutions01@gmail.com>
2026-07-30 20:28:50 +0100

    Add files via upload

---
+++ src/rssapp.d.ts
@@ -0,0 +1,6 @@
+// Type declaration for the rss.app "wall" web component, loaded via <script> in index.html.
+declare namespace JSX {
+  interface IntrinsicElements {
+    "rssapp-wall": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { id?: string };
+  }
+}
